688603d09789a1a406cd9b5cdf54556b206b0dad
Kevin Craig <kevin@getspiffy.com>
2022-11-22 23:20:28 -0500

    adding placeholder content to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Home = () => {
-  return <div className={"bg-warning"}>Home</div>;
+  return (
+    <Container className={"text-center "}>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+        <Col>3</Col>
+      </Row>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+        <Col>3</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
